123c87388f945d3f3579a36557a9844895ff24e3
minje <phone7101@naver.com>
2023-03-20 02:21:38 +0900

    delete files

--- 23-1_Programming-Fundamentals_02/실습02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	char a = 'a';
-
-	printf("%c %c %c\n", a + 1, a + 2, a + 3);
-
-	return 0;
-}
--- 23-1_Programming-Fundamentals_02/연습02.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("\"ASCII code\", \'A\', \'B\', \'C\'\n");
-	printf("\\t \\a \\n\n");
-
-	return 0;
-}
